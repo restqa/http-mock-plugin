0703e301db212b011fd59ea7ca9b79b0edb1d2ba
Olivier <hi@olivierodo.me>
2021-10-29 20:23:28 +0700

    refactor: Increase readability

--- src/http-mock/stubs.js
+++ src/http-mock/stubs.js
@@ -6,12 +6,10 @@ const {Stubby} = require("stubby");
 process.on("message", function (options) {
   options.stubs = options.port;
   options.data = [];
-  for (const [, value] of Object.entries(options.envs)) {
-    const files = glob.sync(`${options.folder}/${value}/**/*.+(yaml|yml)`);
+  for (const [, folder] of Object.entries(options.envs)) {
+    const files = glob.sync(`${options.folder}/${folder}/**/*.+(yaml|yml)`);
     files.reduce((stubs, file) => {
-      const content = fs.readFileSync(file).toString("utf-8");
-      const stub = YAML.parse(content);
-      stub.request.url = "/" + value + stub.request.url;
+      const stub = getStub(folder, file)
       stubs.push(stub);
       return stubs;
     }, options.data);
@@ -19,3 +17,10 @@ process.on("message", function (options) {
   const server = new Stubby();
   server.start(options);
 });
+
+function getStub(folder, file) {
+  const content = fs.readFileSync(file).toString("utf-8");
+  const stub = YAML.parse(content);
+  stub.request.url = "/" + folder + stub.request.url;
+  return stub
+}
